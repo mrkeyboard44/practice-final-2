921a6ab546240e0d22137f7569b1f72a9d0751b1
mrkeyboard44 <matthewdandar@gmail.com>
2021-11-29 22:52:06 -0800

    final commit

--- IOhandler.js
+++ IOhandler.js
@@ -3,11 +3,12 @@
  * File Name: IOhandler.js
  * Description: Collection of functions for files input/output related operations
  *
- * Created Date:
- * Author:
+ * Created Date: Nov 29 / 2021
+ * Author: Matthew Dandar
  *
  */
 
+const { resolve } = require("path");
 const unzipper = require("unzipper"),
   fs = require("fs"),
   PNG = require("pngjs").PNG,
@@ -20,64 +21,67 @@ const unzipper = require("unzipper"),
  * @param {string} pathOut
  * @return {promise}
  */
-const unzip = (pathIn, pathOut) => {
-  fs.createReadStream(pathIn)
-  .pipe(unzipper.Parse())
-  .on('entry', function (entry) {
-    const fileName = entry.path;
-    const type = entry.type; // 'Directory' or 'File'
-    const size = entry.vars.uncompressedSize; // There is also compressedSize;
-    if (fileName === "this IS the file I'm looking for") {
-      entry.pipe(fs.createWriteStream(pathOut));
-    } else {
-      entry.autodrain();
-    }
-  });
-
+const unzip = async (pathIn, pathOut) => {
+  return new Promise((resolve, reject) => {
+    fs.createReadStream(pathIn)
+    .pipe(unzipper.Extract({ path: pathOut }))
+    .on("error", reject)
+    .on("finish", resolve);
+  })
 };
 
 /**
  * Description: read all the png files from given directory and return Promise containing array of each png file path
  *
- * @param {string} path
+ * @param {string} unzippedPath
  * @return {promise}
  */
-const readDir = (dir) => {};
+const readDir = async (unzippedPath) => {
+  return new Promise((resolve, reject) => {
+    let files = fs.readdirSync(unzippedPath)
+    console.log(files)
+    files = files.filter(item => item !== '__MACOSX')
+    files = files.map(item => path.join(unzippedPath, item))
+    console.log(files)
+    resolve(files)
+  })
+};
 
 /**
  * Description: Read in png file by given pathIn,
  * convert to grayscale and write to given pathOut
  *
- * @param {string} filePath
+ * @param {Array} filePaths
  * @param {string} pathProcessed
  * @return {promise}
  */
 
-const grayScale = (pathIn, pathOut) => {
-  fs.createReadStream("in.png")
-  .pipe(
-    new PNG({
-      filterType: 4,
-    })
-  )
-  .on("parsed", function () {
-    for (var y = 0; y < this.height; y++) {
-      for (var x = 0; x < this.width; x++) {
-        var idx = (this.width * y + x) << 2;
- 
-        // invert color
-        var color = (this.data[idx] + this.data[idx + 1] + this.data[idx + 2] )/ 3
-        this.data[idx] = color;
-        this.data[idx + 1] = color;
-        this.data[idx + 2] = color;
- 
-        // and reduce opacity
-        this.data[idx + 3] = this.data[idx + 3] >> 1;
-      }
-    }
- 
-    this.pack().pipe(fs.createWriteStream("out.png"));
-  });
+const grayScale = (filePaths, pathProcessed) => {
+    filePaths.forEach((file, i) => {
+      console.log(file)
+      fs.createReadStream(file)
+      .pipe(
+        new PNG({
+          filterType: 4,
+        })
+      )
+      .on("parsed", function () {
+        for (var y = 0; y < this.height; y++) {
+          for (var x = 0; x < this.width; x++) {
+            var idx = (this.width * y + x) << 2;
+    
+            // make gray
+            var color = (this.data[idx] + this.data[idx + 1] + this.data[idx + 2] )/ 3
+            this.data[idx] = color;
+            this.data[idx + 1] = color;
+            this.data[idx + 2] = color;
+          }
+        }
+        let newPath = path.join(pathProcessed + '/' + i + '.png')
+        this.pack().pipe(fs.createWriteStream(newPath));
+        console.log(`edited file ${i}`)
+      });
+  })
 };
 
 module.exports = {
--- main.js
+++ main.js
@@ -12,7 +12,10 @@ const IOhandler = require("./IOhandler"),
   zipFilePath = `${__dirname}/myfile.zip`,
   pathUnzipped = `${__dirname}/unzipped`,
   pathProcessed = `${__dirname}/grayscaled`;
+console.log(zipFilePath)
 
-IOhandler.unzip()
-.then(() => IOhandler.unzip(zipFilePath, pathUnzipped))
-.then(() => IOhandler.grayScale())
+console.log(__dirname)
+IOhandler.unzip(zipFilePath, pathUnzipped)
+.then(() => IOhandler.readDir(pathUnzipped))
+.then((filePaths) => IOhandler.grayScale(filePaths, pathProcessed))
+
